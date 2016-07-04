17e2a37a4fd09779062e898229f81bf0657b23a9
chayenjr <chayenjr.sk131@gmail.com>
2016-07-04 16:14:52 +0700

    create otp check page and connect fragment

---
+++ app/src/main/java/com/example/chayenjr/digiowallet/OTPpage.java
@@ -0,0 +1,37 @@
+package com.example.chayenjr.digiowallet;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class OTPpage extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    public OTPpage() {
+        // Required empty public constructor
+    }
+
+    public static OTPpage newInstance() {
+        OTPpage fragment = new OTPpage();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_otppage, container, false);
+    }
+}
--- app/src/main/java/com/example/chayenjr/digiowallet/RegisterPage.java
+++ app/src/main/java/com/example/chayenjr/digiowallet/RegisterPage.java
@@ -1,10 +1,16 @@
 package com.example.chayenjr.digiowallet;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterPage extends AppCompatActivity {
+
+    int check_register_page = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,23 @@ public class RegisterPage extends AppCompatActivity {
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.register_actionbar);
+
+        Button nextbutton = (Button)findViewById(R.id.nextbutton);
+        nextbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_register_page == 1){
+                    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.register_info);
+                    if (fragment instanceof OTPpage == false) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.register_info, OTPpage.newInstance(), "OTP Page")
+                                .addToBackStack(null)
+                                .commit();
+                    }else Toast.makeText(RegisterPage.this, "Error", Toast.LENGTH_SHORT).show();
+                    check_register_page = 2;
+                } else{
+                }
+            }
+        });
     }
 }
