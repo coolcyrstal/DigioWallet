fc536387a4e07ad4f0a7a0f5e0087e664930f41f
raveerote <raveerote_hansak@kkumail.com>
2016-07-08 11:38:04 +0700

    main

--- app/src/main/java/com/example/chayenjr/digiowallet/Main/HomePage.java
+++ app/src/main/java/com/example/chayenjr/digiowallet/Main/HomePage.java
@@ -1,15 +1,100 @@
 package com.example.chayenjr.digiowallet.Main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.chayenjr.digiowallet.R;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar mToolbar;
+    NavigationView navigationView;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        initialize();
+        setToolbar();
+        setDrawer();
+
+    }
+
+    private void initialize() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitle("");
+    }
+    private void setDrawer() {
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.profile) {
+
+            // Handle the camera action
+        }else if (id == R.id.setting) {
+
+
+        }else if (id == R.id.source_of_fund) {
+
+        }else if (id == R.id.privacy) {
+
+        }else if (id == R.id.about) {
+
+        }else if (id == R.id.logout) {
+
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
